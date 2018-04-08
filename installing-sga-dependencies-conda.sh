b7c9bb1b55fd3e29e4edfa4fd5b254968ad52548
Julian-Ramirez <julian.ramirez.located@gmail.com>
2018-04-08 16:39:59 -0700

    worked in ubutnu vm

---
+++ installing-sga-dependencies-conda.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Now that the conda package mangager was installed now we can use it!
+# We will now use conda commands to install all of the required dependencies for sga 
+# Note: never run a conda command as "sudo conda" it will mess things up!
+
+
+conda install -c bioconda -y google-sparsehash 
+
+conda install -c bioconda -y bamtools 
+
+conda install -c conda-forge -y zlib 
+
+conda install -c bioconda -y pysam
+
+conda install -c bioconda -y ruffus 
+
+# Now we will install NCBI's tools for downloading short reads. This is called the sra-tools
+
+conda install -c bioconda -y sra-tools
+
+# We are now all ready to run our pipeline! 
